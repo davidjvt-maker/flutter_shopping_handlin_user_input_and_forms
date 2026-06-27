3a9b5ada4c81bc5093dcb47f3880873b26d94879
davidjvt-maker <davidjvieirat.ec@gmail.com>
2026-06-26 22:10:52 -0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_shopping_handlin_user_input_and_forms/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_shopping_handlin_user_input_and_forms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/models/grocery_item.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_shopping_handlin_user_input_and_forms/models/category.dart';
+
+class GroceryItem {
+  const GroceryItem({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.category,
+  });
+
+  final String id;
+  final String name;
+  final int quantity;
+  final Category category;
+}
